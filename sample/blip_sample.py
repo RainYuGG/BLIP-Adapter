f23d62be8f7d4416f616e8562f35b60e988deb47
Ching Yu <r10922141@csie.ntu.edu.tw>
2023-03-29 19:50:04 +0800

    blip sample update

--- sample/blip_sample.py
+++ sample/blip_sample.py
@@ -2,7 +2,7 @@ import torch
 from lavis.models import load_model_and_preprocess
 from PIL import Image
 
-raw_image = Image.open("/data/rico/combined/9511.jpg").convert("RGB")
+raw_image = Image.open("/data/rico/combined/9611.jpg").convert("RGB")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 # loads BLIP caption base model, with finetuned checkpoints on MSCOCO captioning dataset.
@@ -10,8 +10,15 @@ print(device)
 model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_type="base_coco", is_eval=True, device=device)
 # preprocess the image
 # vis_processors stores image transforms for "train" and "eval" (validation / testing / inference)
+# due to only one img, need to unsqueeze to get B domain.
 image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
+text_input = ["a large statue of a person spraying water from a fountain"]
+samples = {"image": image, "text_input": text_input}
+output = model(samples)
+print(output.keys())
+print(output.loss)
+print(output.intermediate_output.image_embeds.shape)
+print(output.intermediate_output.decoder_labels.shape)
 # generate caption
-caption = model.generate({"image": image})
-# ['a large fountain spewing water into the air']
+caption = model.generate({"image": image, "text_input": "a screenshot of"})
 print(caption)
