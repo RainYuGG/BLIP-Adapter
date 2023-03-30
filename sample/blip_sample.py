c74f804523a6f95f8ba5206ed19702e1af7af383
Ching Yu <r10922141@csie.ntu.edu.tw>
2023-03-30 19:13:34 +0800

    blip sample update

--- sample/blip_sample.py
+++ sample/blip_sample.py
@@ -1,3 +1,4 @@
+#%%
 import torch
 from lavis.models import load_model_and_preprocess
 from PIL import Image
@@ -7,18 +8,27 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 # loads BLIP caption base model, with finetuned checkpoints on MSCOCO captioning dataset.
 # this also loads the associated image processors
-model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_type="base_coco", is_eval=True, device=device)
+model, vis_processors, text_processors = load_model_and_preprocess(name="blip_caption", model_type="base_coco", is_eval=False, device=device)
 # preprocess the image
 # vis_processors stores image transforms for "train" and "eval" (validation / testing / inference)
 # due to only one img, need to unsqueeze to get B domain.
-image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
-text_input = ["a large statue of a person spraying water from a fountain"]
+image = vis_processors["train"](raw_image).unsqueeze(0).to(device)
+#%%
+print(text_processors["train"])
+raw_text = "a screenshot of\n"
+print(raw_text)
+print(repr(raw_text))
+text_input = text_processors["train"](raw_text)
+print(repr(text_input))
+#%%
 samples = {"image": image, "text_input": text_input}
 output = model(samples)
 print(output.keys())
 print(output.loss)
+print(output.loss.item())
 print(output.intermediate_output.image_embeds.shape)
 print(output.intermediate_output.decoder_labels.shape)
 # generate caption
-caption = model.generate({"image": image, "text_input": "a screenshot of"})
+caption = model.generate({"image": image, "text_input": text_input})
 print(caption)
+# %%
