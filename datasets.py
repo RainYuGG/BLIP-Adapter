10fd362decf6155839e04aee01b5bc4fc19d20c7
Ching Yu <r10922141@csie.ntu.edu.tw>
2023-04-01 18:05:32 +0800

    random one of five caption for each image for training

--- datasets.py
+++ datasets.py
@@ -5,6 +5,7 @@ import os
 from torchvision.datasets import VisionDataset
 from typing import Any, Callable, Dict, List, Optional, Tuple
 from PIL import Image
+import random
 
 class Screeb2WordsDataset(VisionDataset):
     """
@@ -32,7 +33,7 @@ class Screeb2WordsDataset(VisionDataset):
         super(VisionDataset).__init__()
         self.img_dir = img_dir
         self.caption_file = caption_file
-
+        self.split_type = split_type
         assert split_type in {'TRAIN', 'VALID', 'TEST'}
         if split_type == 'TRAIN':
             split = [int(line.strip()) for line in open(split_dir + 'train_screens.txt', 'r')]
@@ -44,7 +45,7 @@ class Screeb2WordsDataset(VisionDataset):
             split = [int(line.strip()) for line in open(split_dir + 'test_screens.txt', 'r')]
             self.transform = transform['eval']
         self.data = pd.read_csv(caption_file)
-        self.data = self.data[self.data['screenId'].isin(split)].reset_index(drop = True).head(50)
+        self.data = self.data[self.data['screenId'].isin(split)].groupby('screenId').agg(list).reset_index(drop=False)#.head(32)
         #tokenizer
         self.text_processor = text_processor
         
@@ -61,7 +62,10 @@ class Screeb2WordsDataset(VisionDataset):
         img = Image.open(self.img_dir + str(self.data['screenId'][index]) + '.jpg').convert("RGB")
         if self.transform is not None:
             img = self.transform(img)
-        caption = self.data['summary'][index]
+        if self.split_type == 'TRAIN':
+            caption = self.data['summary'][index][random.randint(0, 4)]
+        else:
+            caption = self.data['summary'][index]
         if self.text_processor is not None:
             caption = self.text_processor(caption)  
 
