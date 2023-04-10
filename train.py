156aa3542d894b298be4c2e35577312948694162
Ching Yu <r10922141@csie.ntu.edu.tw>
2023-04-10 15:00:20 +0800

    add gradient accumulation

--- train.py
+++ train.py
@@ -22,12 +22,13 @@ torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
+
 # "cuda" only when GPUs are available.
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#%% 
+
 # set data path
-img_dir = '/data/rico/combined/'
-# img_dir = '/data/rico/convert/'
+# img_dir = '/data/rico/combined/'
+img_dir = '/data/rico/convert/'
 screen2words_dir = '/data/screen2words/'
 caption_file = screen2words_dir + '/screen_summaries.csv'
 split_dir = screen2words_dir + 'split/'
@@ -42,7 +43,7 @@ weight_decay = 0.05
 # Initialize trackers, these are not parameters and should not be changed
 stale = 0
 best_bleu = 0.0
-_exp_name = "bleu_warmup_tfm_e100"
+_exp_name = "bleu_warmup_GA_convert_tfm"
 
 # %%
 model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_type="base_coco", is_eval=False, device=device)
@@ -53,7 +54,6 @@ model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_
 # model.visual_encoder.patch_embed.proj = nn.Conv2d(3, 768, kernel_size=(9, 9), stride=(9, 9))
 # model.visual_encoder.patch_embed.img_size = (224, 224)
 model.to(device)
-
 
 # %%
 # training preprocessor
@@ -77,32 +77,44 @@ num_warmup_steps = int(0.1 * num_training_steps)
 scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_training_steps)
 
 #%%
+# Gradient Accumulation
+accumulation_steps = 4
+bs = batch_size * accumulation_steps
 for epoch in range(num_epochs):
     # ---------- Training ----------
     model.train()
     train_loss = []
-    for batch in tqdm(train_loader):
+    for index, batch in enumerate(tqdm(train_loader)):
         batch['image'] = batch['image'].to(device)
         # print(batch['image_id'],batch['text_input'])
         batch['text_input'] = batch['text_input'][random.randint(0,4)]
+
         # Forward the data. (Make sure data and model are on the same device.)
         output = model(batch)
-        # bert using gelu and Cross Entropy for loss function automatically.
-        # model output contain the cross-entropy loss.
-        loss = output.loss
-        # Gradients stored in the parameters in the previous step should be cleared out first.
-        optimizer.zero_grad()
+        loss = output.loss / accumulation_steps
+
+        # Bert using gelu and Cross Entropy for loss function automatically.
+        # model output contain the Cross Entropy loss.
         # Compute the gradients for parameters.
         loss.backward()
-        # Clip the gradient norms for stable training.
-        # grad_norm = nn.utils.clip_grad_norm_(model.parameters(), max_norm=10.0)
-        # Update the parameters with computed gradients.
-        optimizer.step()
-        # Update the learning rate with scheduler
-        scheduler.step()
-        # Record the loss and accuracy.
+
+        # Gradient Accumulation
+        if (index + 1) % accumulation_steps == 0:
+            # Clip the gradient norms for stable training.
+            # grad_norm = nn.utils.clip_grad_norm_(model.parameters(), max_norm=10.0)
+
+            # Update the parameters with computed gradients.
+            optimizer.step()
+
+            # Update the learning rate with scheduler
+            scheduler.step()
+
+            # Gradients stored in the parameters in the previous step should be cleared out first.
+            optimizer.zero_grad()
+
+        # Record the loss.
         train_loss.append(loss.item())
-        
+
     train_loss = sum(train_loss) / len(train_loss)
     # Print the information.
     print(f"[ Train | {epoch + 1:03d}/{num_epochs:03d} ] loss = {train_loss:.5f}")
@@ -128,15 +140,15 @@ for epoch in range(num_epochs):
     print(f"[ Valid | {epoch + 1:03d}/{num_epochs:03d} ] bleu = {valid_bleu:.5f}, CIDEr = {total_score['CIDEr']:.5f}, RougeL = {total_score['ROUGE_L']:.5f}")
     # update logs
     if valid_bleu > best_bleu:
-        with open(f"./log/{_exp_name}_bs{batch_size}_log.txt","a") as f:
-            f.write(f"[ Valid | {epoch + 1:03d}/{num_epochs:03d} ] bleu = {valid_bleu:.5f} -> best\n")
+        with open(f"./log/{_exp_name}_bs{bs}_log.txt","a") as f:
+            f.write(f"[ Valid | {epoch + 1:03d}/{num_epochs:03d} ] bleu = {valid_bleu:.5f}, CIDEr = {total_score['CIDEr']:.5f}, RougeL = {total_score['ROUGE_L']:.5f} -> best\n")
     else:
-        with open(f"./log/{_exp_name}_bs{batch_size}_log.txt","a") as f:
-            f.write(f"[ Valid | {epoch + 1:03d}/{num_epochs:03d} ] bleu = {valid_bleu:.5f}\n")
+        with open(f"./log/{_exp_name}_bs{bs}_log.txt","a") as f:
+            f.write(f"[ Valid | {epoch + 1:03d}/{num_epochs:03d} ] bleu = {valid_bleu:.5f}, CIDEr = {total_score['CIDEr']:.5f}, RougeL = {total_score['ROUGE_L']:.5f}\n")
     # save models
     if valid_bleu > best_bleu:
         print(f"Best model found at epoch {epoch}, saving model")
-        torch.save(model.state_dict(), f"{_exp_name}_bs{batch_size}.ckpt") # only save best to prevent output memory exceed error
+        torch.save(model.state_dict(), f"{_exp_name}_bs{bs}.ckpt") # only save best to prevent output memory exceed error
         best_bleu = valid_bleu
         stale = 0
     else:
