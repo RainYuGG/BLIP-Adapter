dc27f53639948d0709502d006d92a7761c548d4c
Ching Yu <r10922141@csie.ntu.edu.tw>
2023-04-10 18:24:36 +0800

    use multi score metric(BLEU, CIDEr) for validation

--- train.py
+++ train.py
@@ -27,8 +27,8 @@ torch.backends.cudnn.benchmark = False
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # set data path
-# img_dir = '/data/rico/combined/'
-img_dir = '/data/rico/convert/'
+img_dir = '/data/rico/combined/'
+# img_dir = '/data/rico/convert/'
 screen2words_dir = '/data/screen2words/'
 caption_file = screen2words_dir + '/screen_summaries.csv'
 split_dir = screen2words_dir + 'split/'
@@ -36,14 +36,14 @@ split_dir = screen2words_dir + 'split/'
 # set hyperparameters
 # parameter for training 
 num_epochs = 100
-patience = 30
+patience = 10
 batch_size = 32
 learning_rate = 1e-4
 weight_decay = 0.05
 # Initialize trackers, these are not parameters and should not be changed
 stale = 0
-best_bleu = 0.0
-_exp_name = "bleu_warmup_GA_convert_tfm"
+best_score = 0.0
+_exp_name = "bleu_warmup_GA_tfm_fullreshape"
 
 # %%
 model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_type="base_coco", is_eval=False, device=device)
@@ -115,7 +115,7 @@ for epoch in range(num_epochs):
         # Record the loss.
         train_loss.append(loss.item())
 
-    train_loss = sum(train_loss) / len(train_loss)
+    train_loss = accumulation_steps * sum(train_loss) / len(train_loss)
     # Print the information.
     print(f"[ Train | {epoch + 1:03d}/{num_epochs:03d} ] loss = {train_loss:.5f}")
 
@@ -132,24 +132,24 @@ for epoch in range(num_epochs):
         with torch.no_grad():
             caption_pred = model.generate(img_input)
             caption_predictions += caption_pred
-    valid_bleu = scorer.calculate_score(caption_predictions, caption_references, 'bleu')
+    # valid_score = scorer.calculate_score(caption_predictions, caption_references, 'bleu')
     cocoeval = scorer.Scorers(caption_predictions, caption_references)
     total_score = cocoeval.compute_scores()
-
     # Print the information.
-    print(f"[ Valid | {epoch + 1:03d}/{num_epochs:03d} ] bleu = {valid_bleu:.5f}, CIDEr = {total_score['CIDEr']:.5f}, RougeL = {total_score['ROUGE_L']:.5f}")
+    print(f"[ Valid | {epoch + 1:03d}/{num_epochs:03d} ] bleu = {total_score['bleu'][3]:.5f}, CIDEr = {total_score['CIDEr']:.5f}, RougeL = {total_score['ROUGE_L']:.5f}")
     # update logs
-    if valid_bleu > best_bleu:
+    valid_score = total_score['bleu'][3] + total_score['CIDEr']
+    if valid_score > best_score:
         with open(f"./log/{_exp_name}_bs{bs}_log.txt","a") as f:
-            f.write(f"[ Valid | {epoch + 1:03d}/{num_epochs:03d} ] bleu = {valid_bleu:.5f}, CIDEr = {total_score['CIDEr']:.5f}, RougeL = {total_score['ROUGE_L']:.5f} -> best\n")
+            f.write(f"[ Valid | {epoch + 1:03d}/{num_epochs:03d} ] bleu = {total_score['bleu'][3]:.5f}, CIDEr = {total_score['CIDEr']:.5f}, RougeL = {total_score['ROUGE_L']:.5f} -> best\n")
     else:
         with open(f"./log/{_exp_name}_bs{bs}_log.txt","a") as f:
-            f.write(f"[ Valid | {epoch + 1:03d}/{num_epochs:03d} ] bleu = {valid_bleu:.5f}, CIDEr = {total_score['CIDEr']:.5f}, RougeL = {total_score['ROUGE_L']:.5f}\n")
+            f.write(f"[ Valid | {epoch + 1:03d}/{num_epochs:03d} ] bleu = {total_score['bleu'][3]:.5f}, CIDEr = {total_score['CIDEr']:.5f}, RougeL = {total_score['ROUGE_L']:.5f}\n")
     # save models
-    if valid_bleu > best_bleu:
+    if valid_score > best_score:
         print(f"Best model found at epoch {epoch}, saving model")
         torch.save(model.state_dict(), f"{_exp_name}_bs{bs}.ckpt") # only save best to prevent output memory exceed error
-        best_bleu = valid_bleu
+        best_score = valid_score
         stale = 0
     else:
         stale += 1
