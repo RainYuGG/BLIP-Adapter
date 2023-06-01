3411d513e4120e60c9596d3c5402b9abed9bee1f
Ching Yu <r10922141@csie.ntu.edu.tw>
2023-06-02 02:35:58 +0800

    [refactor] change tuning option of adapter and language model in the config

--- loader.py
+++ loader.py
@@ -16,12 +16,14 @@ def load_model(model_name: str):
         model.load_checkpoint(config['model']['checkpoint_url'])
 
     # freeze all parameters except prompt and language model
-    if 'adapter_type' in config['model']['args'].keys():
-        for name, param in model.named_parameters():
-            if "prompt" not in name and "text" not in name:
-                param.requires_grad_(False)
-            else:
-                print(name)
+    for name, param in model.named_parameters():
+        if 'adapter_type' in config['model']['args'].keys() and "prompt" in name:
+            print(name)
+        elif config['model']['args']['tune_language'] and "text" not in name:
+            print(name)
+        else:
+            param.requires_grad_(False)
+            
                 
     return model
 
